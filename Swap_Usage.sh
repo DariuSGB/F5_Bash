4f76b0c7ac859c3570b93292f328ebb7d6bda861
Dario Garrido <55515143+DariuSGB@users.noreply.github.com>
2020-04-12 19:34:41 +0200

    Create Swap_Usage.sh

---
+++ Swap_Usage.sh
@@ -0,0 +1,8 @@
+#########################################################################
+# title: Swap_Usage.sh                                                  #
+# author: Dario Garrido                                                 #
+# date: 20200410                                                        #
+# description: Show swap memory used by processes                       #
+#########################################################################
+
+for file in /proc/*/status ; do awk '/VmSwap|Name/{printf $2 " " $3}END{ print ""}' $file; done | sort -k 2 -n -r | less
