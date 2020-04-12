90b83ef58dcc2e8740bc0858bde4e8aac00f88b7
Dario Garrido <55515143+DariuSGB@users.noreply.github.com>
2020-04-12 19:42:07 +0200

    Update Memory_Usage.sh

--- Memory_Usage.sh
+++ Memory_Usage.sh
@@ -5,15 +5,4 @@
 # description: Show memory usage in percentage (%)                      #
 #########################################################################
 
-awk '
-BEGIN {
-	while (("free" | getline) > 0 ) {
-	    if ($1 == "Mem:") {
-	        print("\nMem Used:\t\t", ($3 / $2) * 100)
-	    } else if ($2 ~ "buffers") {
-	        print("Used After Buffers:\t", ($4 / $3) * 100)
-	    } else if($1 == "Swap:") {
-	        print("Swap Used:\t\t", ($3 / $2) * 100)
-	    }
-	} close("free")
-}'
+awk 'BEGIN { while (("free" | getline) > 0 ) { if ($1 == "Mem:") { print("\nMem Used:\t\t", ($3 / $2) * 100) } else if ($2 ~ "buffers") { print("Used After Buffers:\t", ($4 / $3) * 100) } else if($1 == "Swap:") { print("Swap Used:\t\t", ($3 / $2) * 100) } } close("free") }'
